6802b0776622aa8851626e3bf0b3817173a6ea71
davidmiller11 <davidmiller11@gmail.com>
2014-05-31 22:06:34 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // app.js
 
-$( document ).ready(function() {
+$(document).ready(function() {
   // $('#my-name').hide();
   // $('#my-name').addClass('animated');
   // window.setTimeout(bounceIn, 2000);
@@ -15,3 +15,29 @@ function bounceIn() {
 function bounceOut() {
   $('#my-name').addClass('bounceOut');
 }
+
+$(document).ready(function() {
+    $('section[data-type="background"]').each(function() {
+        var $bgobj = $(this); // assigning the object
+     
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+             
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+ 
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+});
+
+$(document).ready(function(){
+  $('.single-item').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1
+  });
+});
