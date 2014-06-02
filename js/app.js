88c7561cfbdbb2913a7b78eac6c86c188dbaea83
davidmiller11 <davidmiller11@gmail.com>
2014-06-01 20:44:13 -0400

    Add rotate to picture on scroll

--- js/app.js
+++ js/app.js
@@ -1,37 +1,5 @@
 // app.js
 
-$(document).ready(function() {
-  // $('#my-name').hide();
-  // $('#my-name').addClass('animated');
-  // window.setTimeout(bounceIn, 2000);
-  // window.setTimeout(bounceOut, 4000);
-});
-
-function bounceIn() {
-  $('#my-name').show();
-  $('#my-name').addClass('bounceIn');
-}
-
-function bounceOut() {
-  $('#my-name').addClass('bounceOut');
-}
-
-$(document).ready(function() {
-    $('section[data-type="background"]').each(function() {
-        var $bgobj = $(this); // assigning the object
-     
-        $(window).scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-             
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
- 
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        }); 
-    });    
-});
-
 $(document).ready(function(){
   $('.single-item').slick({
       dots: true,
@@ -42,4 +10,13 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1
   });
+
+  scrollDist = 0;
+
+  window.onscroll = function (event) {
+    scrollDist = $('body').scrollTop();
+    console.log(scrollDist);
+    $('#my-headshot').css('transform','rotate('+scrollDist+'deg)');
+  };
+
 });
